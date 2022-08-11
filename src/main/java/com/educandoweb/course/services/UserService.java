0c8e2051ccd1a5515bfc9547b2f0961e922529db
Eduardo <edusousavieira@gmail.com>
2022-08-11 10:57:53 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,10 +23,26 @@ public class UserService {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
+
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateDate(entity, obj);
+		return repository.save(entity);
+
+	}
+
+	private void updateDate(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
 }
